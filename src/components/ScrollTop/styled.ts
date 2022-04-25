4ddd37d9c5edda555fba0c2175c6e1c45915dbcb
byeongyong.kang <byeongyong.kang@vuno.co>
2022-04-25 15:54:49 +0900

    change scroll position

--- src/components/ScrollTop/styled.ts
+++ src/components/ScrollTop/styled.ts
@@ -2,7 +2,7 @@ import tw from 'tailwind-styled-components'
 
 export const Button = tw.button`
   p-4 text-zinc-900 dark:text-zinc-50 font-medium text-xs leading-tight uppercase rounded-full shadow-md
-  transition duration-150 ease-in-out hidden bottom-8 right-8 fixed
+  transition duration-150 ease-in-out hidden bottom-16 right-8 fixed
  hover:bg-gray-300 hover:shadow-lg
  active:bg-yellow-500 active:shadow-lg 
  focus:bg-yellow-500 focus:shadow-lg focus:outline-none focus:ring-0
