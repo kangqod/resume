a5265494073f0ba88f9965773cce4ea9227f2a4d
byeongyong.kang <byeongyong.kang@vuno.co>
2022-04-28 16:45:33 +0900

    add li margin

--- src/components/Home/About/styled.ts
+++ src/components/Home/About/styled.ts
@@ -6,7 +6,7 @@ pt-8 px-8
 `
 
 export const Image = tw.img`
-hidden md:block w-4/5 h-4/5 md:w-64 md:h-64 lg:w-96 lg:h-96
+hidden md:block w-auto h-4/5 md:h-64 lg:h-96
 `
 export const Description = tw.h2`
 font-bold text-lg sm:text-xl md:text-xl leading-10 sm:leading-10 md:leading-9 lg:leading-15 text-zinc-900 dark:text-zinc-50
@@ -16,13 +16,16 @@ export const TextHighlight = tw.span`
 text-yellow-600
 `
 export const Top = tw.div`
-flex gap-x-5 lg:gap-x-10
+flex gap-x-12 lg:gap-x-20
 `
 export const Bottom = tw.div`
 py-4 px-8 md:p-3
 `
 export const Ul = tw.ul`
 list-disc text-zinc-900 dark:text-zinc-50
+`
+export const Li = tw.li`
+mt-1 md:mt-2
 `
 export const Text = tw.p`
 text-zinc-900 dark:text-zinc-50
