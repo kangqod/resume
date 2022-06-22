21cb977d9bbdbd34edc589d35d589dfc9d936b4b
byeongyong.kang <byeongyong.kang@vuno.co>
2022-06-22 15:15:09 +0900

    add test

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Home from '@/components/Home'
 import { ThemeProvider } from '@/context/theme'
 import ScrollTop from '@/components/ScrollTop'
 
+const t = 't'
+
 function App() {
   return (
     <ThemeProvider>
